13ed66e3f1b4fe38df215bf66c8c6f9c81a55d9b
Elidrissi <83175070+R1i4ck@users.noreply.github.com>
2022-11-04 22:03:39 +0100

    Python Port scanner

---
+++ Project1.py
@@ -0,0 +1,27 @@
+from socket import *
+def conScan(tgHost, tgPort):
+    try:
+        connect = socket(AF_INET, SOCK_STREAM)
+        connect.connect((tgHost, tgPort))
+        print("[+]%d/tcp open"% tgPort)
+        connect.close()
+    except:
+        print("[-]%d/tcp closed"% tgPort)
+def portScan(tgHost, tgPorts):
+    try:
+        tgtIP = gethostbyname(tgHost)
+    except:
+        print('[-] Cannot resolve %d "% tgPort')
+        return
+    try:
+        tgtname = gethostbyaddr(tgtIP)
+        print('/n[-] scan result of: %d "% tgname[0]')
+    except:
+         print('/n[-] scan result of: %d "% tgIP[0]')
+    setdefaulttimeout(1)
+    for tgPort in tgPorts:
+        print('Scanning Port: %d'% tgPort)
+        conScan(tgHost, int(tgPort))
+if __name__ == '__main__':
+    portScan("xnxx.com", [80, 22])
+    
